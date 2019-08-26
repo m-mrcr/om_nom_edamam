cc4934645312df066ae847c12fef5892b8bbe080
earl-stephens <stephensearl@hotmail.com>
2019-08-26 15:29:53 -0600

    Minor changes to fetch call to Edamam

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -11,28 +11,28 @@ router.get('/food_search', function(req,res) {
   sequelize.query('SELECT * FROM "Recipes" WHERE "title" ILIKE ?', {raw: true, replacements: [`%${foodType}%`]})
   .then(recipes => {
     if (recipes[1].rowCount == 0) {
-      // fetch(`https://api.edamam.com/search?q=${foodType}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}, {compress: true, headers: {"Accept-Encoding": "gzip"}}`)
-      fetch(`https://api.edamam.com/search?q=${foodType}&app_id=02463f36&app_key=4787d094f95af4ea067a8df3757c3e2d`, {compress: true, headers: {"Accept-Encoding": "gzip"}})
+      fetch(`https://api.edamam.com/search?q=${foodType}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`, {compress: true, headers: {"Accept-Encoding": "gzip"}})
       .then(res => res.json())
       .then(json => {
         var edamamData = json.hits;
-        edamamData.forEach(function(element) {
-          var servingCalories = element.recipe.calories / element.recipe.yield
-          Recipe.create({
-            title: element.recipe.title,
-            cookTime: element.recipe.cookTime,
-            caloriesPerServing: parseInt(servingCalories),
-            servingAmount: element.recipe.yield,
-            image: element.recipe.image,
-            url: element.recipe.url,
-            healthDetails: JSON.stringify(element.recipe.healthLabels),
-            ingredients: JSON.stringify(element.recipe.ingredientLines)
-          })
-        })
+        console.log(edamamData);
+        // edamamData.forEach(function(element) {
+        //   var servingCalories = element.recipe.calories / element.recipe.yield
+        //   Recipe.create({
+        //     title: element.recipe.title,
+        //     cookTime: element.recipe.cookTime,
+        //     caloriesPerServing: parseInt(servingCalories),
+        //     servingAmount: element.recipe.yield,
+        //     image: element.recipe.image,
+        //     url: element.recipe.url,
+        //     healthDetails: JSON.stringify(element.recipe.healthLabels),
+        //     ingredients: JSON.stringify(element.recipe.ingredientLines)
+        //   })
+        // })
       })
-      .then(updatedData => {
-        return sequelize.query('SELECT * FROM "Recipes" WHERE "title" ILIKE ?', {raw: true, replacements: [`%${foodType}%`]})
-      })
+      // .then(updatedData => {
+      //   return sequelize.query('SELECT * FROM "Recipes" WHERE "title" ILIKE ?', {raw: true, replacements: [`%${foodType}%`]})
+      // })
       .then(returnData => {
         return console.log(returnData);
         // res.setHeader("Content-Type", "application/json");
