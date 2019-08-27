86bbe21a8543438b8c13a9cce75a427c98650de4
earl-stephens <stephensearl@hotmail.com>
2019-08-26 22:53:23 -0600

    Added CORS to the app

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config()
+var app = express();
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var recipeRouter = require('./routes/api/v1/recipes');
-var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+});
 
 app.use('/', indexRouter);
 app.use('/api/v1/recipes', recipeRouter);
