50fd78146f27d83ee0dbe903ec14a89d86759a53
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-08-28 14:21:22 -0600

    adds implementation for time_sort endpoint

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -100,4 +100,74 @@ router.get('/health_search', function(req,res) {
   });
 });
 
+router.get('/time_sort', function(req,res) {
+  var foodType = req.query.q;
+  var capitalizedFoodType = foodType.charAt(0).toUpperCase() + foodType.slice(1);
+  Recipe.findAll({
+    where: {
+      title: {
+        [Op.substring]: `${capitalizedFoodType}`
+      }
+    },
+    order: [
+      ['cookTime', 'ASC']
+    ]
+  })
+  .then(recipes => { console.log(recipes)
+    if (recipes[0] == undefined) {
+      fetch(`https://api.edamam.com/search?q=${foodType}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`, {compress: true, headers: {"Accept-Encoding": "gzip"}})
+      .then(res => res.json())
+      .then(json => {
+        return json.hits })
+      .then(edamamData => {
+        var formatData = []
+         edamamData.forEach(function(element) {
+          var servingCalories = element.recipe.calories / element.recipe.yield
+          var e = {
+            title: element.recipe.label,
+            cookTime: element.recipe.totalTime,
+            caloriesPerServing: parseInt(servingCalories),
+            servingAmount: element.recipe.yield,
+            image: element.recipe.image,
+            url: element.recipe.url,
+            healthDetails: JSON.stringify(element.recipe.healthLabels),
+            ingredients: JSON.stringify(element.recipe.ingredientLines)
+          }
+          formatData.push(e)
+        })
+        return formatData
+      })
+      .then(formattedData => {
+        Recipe.bulkCreate(formattedData)
+      })
+      .then(created => {
+        return Recipe.findAll({
+          where: {
+            title: {
+              [Op.substring]: `${capitalizedFoodType}`
+            }
+          },
+          order: [
+            ['cookTime', 'ASC']
+          ]
+        })
+      })
+      .then(result => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).send(JSON.stringify(result));
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({error});
+      });
+    } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(recipes));
+    }})
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({error});
+  });
+});
+
 module.exports = router;
