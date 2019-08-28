539fbd1748f6c487defc908ff36dda5dc222701c
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-08-28 10:30:15 -0600

    refines tests for recipe calories_search and food_search

--- test/recipe.test.js
+++ test/recipe.test.js
@@ -17,13 +17,32 @@ describe('Recipe', () => {
     shell.exec('npx sequelize db:migrate:undo:all')
   });
 
-  it('GET recipes for a food type', () => {
+  it('GET recipes for a food type - IN DB', () => {
     return request(app)
     .get('/api/v1/recipes/food_search?q=beef')
     .set("Content-Type", "application/json")
     .set("Accept", "application/json")
-    .then(response => {console.log(response);
+    .then(response => {
       expect(response.body.length).toBe(5)
+      expect(Object.keys(response.body[0])).toContain('title')
+      expect(Object.keys(response.body[0])).toContain('cookTime')
+      expect(Object.keys(response.body[0])).toContain('caloriesPerServing')
+      expect(Object.keys(response.body[0])).toContain('servingAmount')
+      expect(Object.keys(response.body[0])).toContain('image')
+      expect(Object.keys(response.body[0])).toContain('url')
+      expect(Object.keys(response.body[0])).toContain('healthDetails')
+      expect(Object.keys(response.body[0])).toContain('ingredients')
+    })
+  });
+
+  it('GET recipes for a food type - NOT IN DB', () => {
+    return request(app)
+    .get('/api/v1/recipes/food_search?q=pear')
+    .set("Content-Type", "application/json")
+    .set("Accept", "application/json")
+    .then(response => {
+      expect(response.statusCode).toBe(201)
+      expect(response.body.length).toBe(10)
       expect(Object.keys(response.body[0])).toContain('title')
       expect(Object.keys(response.body[0])).toContain('cookTime')
       expect(Object.keys(response.body[0])).toContain('caloriesPerServing')
@@ -37,7 +56,7 @@ describe('Recipe', () => {
 
   fit('GET recipes for a food type - SADPATH', () => {
     return request(app)
-    .get('/api/v1/recipes/food_search?q=')
+    .get('/api/v1/recipes/food_search?q=[]')
     .set("Content-Type", "application/json")
     .set("Accept", "application/json")
     .then(response => {
@@ -51,7 +70,8 @@ describe('Recipe', () => {
     .set("Content-Type", "application/json")
     .set("Accept", "application/json")
     .then(response => {
-      expect(response.body.length).toBe(16)
+      expect(response.statusCode).toBe(200)
+      expect(response.body.length).toBe(6)
       expect(Object.keys(response.body[0])).toContain('title')
       expect(Object.keys(response.body[0])).toContain('cookTime')
       expect(Object.keys(response.body[0])).toContain('caloriesPerServing')
@@ -65,7 +85,7 @@ describe('Recipe', () => {
 
   it('GET recipes for range of calories - SADPATH', () => {
     return request(app)
-    .get('/api/v1/recipes/calorie_search?q=150')
+    .get('/api/v1/recipes/calorie_search?q=')
     .set("Content-Type", "application/json")
     .set("Accept", "application/json")
     .then(response => {
